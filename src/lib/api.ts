6c416cc321cf44df0f359e4023705c7fc1f18870
DarshanKGithub <darshankshetri8@gmail.com>
2025-12-17 18:40:08 +0530

    api changes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,17 +1,18 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// Attach token automatically (if exists)
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
   }
   return config;
 });
